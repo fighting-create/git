7d23ba1f4476fda579e5a602f74a2dde668a4716
Xin <654034573@qq.com>
2020-10-15 21:41:02 +0800

    版本5

--- src/hello.java
+++ src/hello.java
@@ -6,5 +6,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("版本1");
         System.out.println("版本2");
+        System.out.println("版本5");
     }
 }
